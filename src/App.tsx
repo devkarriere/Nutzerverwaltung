e8310de77c988a4c98dbc27dfb3d5523eb7ac720
Liam Goodman <liam.goodman@outlook.de>
2024-04-23 00:25:45 +0200

    fix: Update Baseroute for Router

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { User } from "./types/User";
 
 const router = createBrowserRouter([
   {
-    path: "/Nutzerverwaltung",
+    path: "/",
     element: <Root />,
     children: [
       { path: "overview", element: <Overview /> },
